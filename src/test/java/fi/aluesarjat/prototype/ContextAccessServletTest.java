4b91e934d98387710cfc0f94ff1b7db9414e9170
Timo Westkämper <timo.westkamper@mysema.com>
2010-11-09 08:13:40 +0000

    added tests

--- src/test/java/fi/aluesarjat/prototype/ContextAccessServletTest.java
+++ src/test/java/fi/aluesarjat/prototype/ContextAccessServletTest.java
@@ -1,27 +1,55 @@
 package fi.aluesarjat.prototype;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
+import com.mysema.rdfbean.model.LIT;
+import com.mysema.rdfbean.model.RDF;
+import com.mysema.rdfbean.model.RDFConnection;
+import com.mysema.rdfbean.model.RDFS;
+import com.mysema.rdfbean.model.STMT;
+import com.mysema.rdfbean.model.UID;
+import com.mysema.rdfbean.model.io.Format;
 import com.mysema.rdfbean.sesame.MemoryRepository;
 
 public class ContextAccessServletTest {
 
     private static MemoryRepository repository;
     
+    private ContextAccessServlet servlet;
+    
+    private MockHttpServletRequest request;
+    
+    private MockHttpServletResponse response;
+    
     @BeforeClass
     public static void setUpClass() throws ServletException{
         repository = new MemoryRepository();
-        repository.initialize();                
+        repository.initialize();
+        RDFConnection connection = repository.openConnection();
+        try{
+            UID uid = new UID("http://localhost:80/rdf/test");
+            Set<STMT> stmts = new HashSet<STMT>();
+            stmts.add(new STMT(uid, RDF.type, RDFS.Class, uid));
+            stmts.add(new STMT(uid, RDFS.label, new LIT("label"), uid));
+            connection.update(Collections.<STMT>emptySet(), stmts);
+        }finally{
+            connection.close();
+        }
     }
     
     @AfterClass
@@ -29,13 +57,41 @@ public class ContextAccessServletTest {
         repository.close();
     }
     
+    @Before
+    public void setUp(){
+        servlet = new ContextAccessServlet(repository);
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+    }
+    
     @Test
-    public void Get() throws ServletException, IOException{
-        ContextAccessServlet servlet = new ContextAccessServlet(repository);
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        MockHttpServletResponse response = new MockHttpServletResponse();
+    public void Get_Available_Context() throws ServletException, IOException{
+        request.setRequestURI("/rdf/test");       
         servlet.service(request, response);
+        
         assertTrue(response.getContentAsString().contains("rdf:RDF"));
+        assertTrue(response.getContentAsString().contains("type"));
+        assertTrue(response.getContentAsString().contains("label"));
+    }
+    
+    @Test
+    public void Get_Available_Context_as_Turtle() throws ServletException, IOException{
+        request.setRequestURI("/rdf/test");
+        request.addHeader("Accept", Format.TURTLE.getMimetype());
+        servlet.service(request, response);
+        
+        assertFalse(response.getContentAsString().contains("rdf:RDF"));
+        assertTrue(response.getContentAsString().contains(" a "));
+    }
+    
+    @Test
+    public void Get_Unavailable_Context() throws ServletException, IOException{
+        request.setRequestURI("/rdf/unknown");        
+        servlet.service(request, response);
+        
+        assertTrue(response.getContentAsString().contains("rdf:RDF"));
+        assertFalse(response.getContentAsString().contains("type"));
+        assertFalse(response.getContentAsString().contains("label"));
     }
     
 }
