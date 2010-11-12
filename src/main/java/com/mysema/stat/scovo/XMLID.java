60b74c4c5202a6f1249a4d411f8683726d9f2fec
Timo Westkämper <timo.westkamper@mysema.com>
2010-11-12 14:53:39 +0000

    simplified XMLID

--- src/main/java/com/mysema/stat/scovo/XMLID.java
+++ src/main/java/com/mysema/stat/scovo/XMLID.java
@@ -1,99 +1,42 @@
 package com.mysema.stat.scovo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+
+import org.apache.commons.lang.CharSet;
 
 import com.mysema.commons.lang.Assert;
 
 public final class XMLID {
 
     private static final Pattern WHITESPACE = Pattern.compile("\\s+");
-    
-    public static class CharRange {
-        private char start;
-        private char end;
-        public CharRange(char ch) {
-            this(ch, ch);
-        }
-        public CharRange(char start, char end) {
-            Assert.assertThat(start <= end, "invalid range", null, null);
-            this.start = start;
-            this.end = end;
-        }
-        public boolean contains(char ch) {
-            return start <= ch && ch <= end;
-        }
+
+    private static final CharSet ID_START_CHARS, ID_CHARS;
+
+    static{
+        List<String> idStartChars = Arrays.asList(
+                "A-Z", "_", "a-z",
+                "\u00C0-\u00D6", "\u00D8-\u00F6",
+                "\u00F8-\u02FF", "\u0370-\u037D",
+                "\u037F-\u1FFF", "\u200C-\u200D",
+                "\u2070-\u218F", "\u2C00-\u2FEF",
+                "\u3001-\uD7FF", "\uF900-\uFDCF",
+                "\uFDF0-\uFDCF", "\uFDF0-\uFFFD");
+
+       List<String> idChars = new ArrayList<String>(idStartChars);
+       idChars.addAll(Arrays.asList(
+               "-", "0-9",
+               "\u00B7", "\u0300-\u036F",
+               "\u203F-\u2040"));
+
+       ID_START_CHARS = CharSet.getInstance(idStartChars.toArray(new String[idStartChars.size()]));
+       ID_CHARS = CharSet.getInstance(idChars.toArray(new String[idChars.size()]));
+
+
     }
-    
-    public static class CharRanges {
-        private CharRanges[] nestedRanges;
-        private CharRange[] ranges;
-        public CharRanges(CharRanges... nestedRanges) {
-            this(nestedRanges, (CharRange[]) null);
-        }
-        public CharRanges(CharRange... ranges) {
-            this((CharRanges[]) null, ranges);
-        }
-        public CharRanges(CharRanges nestedRange, CharRange... ranges) {
-            this(new CharRanges[] {nestedRange}, ranges);
-        }
-        public CharRanges(CharRanges[] nestedRanges, CharRange... ranges) {
-            this.nestedRanges = nestedRanges;
-            this.ranges = ranges;
-        }
-        public boolean contains(char ch) {
-            if (nestedRanges != null) {
-                for (CharRanges r : nestedRanges) {
-                    if (r.contains(ch)) {
-                        return true;
-                    }
-                }
-            }
-            if (ranges != null) {
-                for (CharRange range : ranges) {
-                    if (range.contains(ch)) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-    }
-    
-    public static CharRange range(char ch) {
-        return new CharRange(ch);
-    }
-    
-    public static CharRange range(char start, char end) {
-        return new CharRange(start, end);
-    }
-    
-    private static final CharRanges ID_START_CHARS = new CharRanges(
-            range('A', 'Z'),
-            range('_'),
-            range('a', 'z'),
-            range('\u00C0', '\u00D6'),
-            range('\u00D8', '\u00F6'), 
-            range('\u00F8', '\u02FF'), 
-            range('\u0370', '\u037D'), 
-            range('\u037F', '\u1FFF'), 
-            range('\u200C', '\u200D'), 
-            range('\u2070', '\u218F'), 
-            range('\u2C00', '\u2FEF'), 
-            range('\u3001', '\uD7FF'), 
-            range('\uF900', '\uFDCF'), 
-            range('\uFDF0', '\uFFFD')
-    );
- 
-    private static final CharRanges ID_CHARS = new CharRanges(
-            ID_START_CHARS,
-            range('-'),
-//            range('.'), // Causes problems in Turtle and Sparql
-            range('0', '9'),
-            range('\u00B7'),
-            range('\u0300', '\u036F'),
-            range('\u203F', '\u2040')
-    );
-    
+
     public static String toXMLID(String name) {
         Assert.hasLength(name, "name");
 
@@ -111,7 +54,7 @@ public final class XMLID {
                 sb.append(encode(chars[0]));
             }
         }
-        
+
         for (int i=1; i < chars.length; i++) {
             if (ID_CHARS.contains(chars[i])) {
                 sb.append(chars[i]);
@@ -127,11 +70,11 @@ public final class XMLID {
     private static boolean replaceWithUnderscore(char ch) {
         return Character.isWhitespace(ch) || '.' == ch;
     }
-    
+
     private static String encode(char ch) {
         return Integer.toString(ch, Character.MAX_RADIX);
     }
-    
+
     private XMLID(){}
-    
+
 }
