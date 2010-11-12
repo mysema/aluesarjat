81c28a317d915c3df2d05a0b6f9aa6279187ddc6
Samppa Saarela <samppa.saarela@mysema.com>
2010-11-12 14:57:02 +0000

    Improved UI

--- src/main/webapp/scripts/queryform.js
+++ src/main/webapp/scripts/queryform.js
@@ -1,4 +1,7 @@
 namespaces = {};
+
+String.prototype.startsWith = function(str) {return (this.match("^"+str)==str)}
+
 $(document).ready(function(){
 	var query = "SELECT ?ns ?prefix WHERE { ?ns <http://data.mysema.com/schemas/meta#nsPrefix> ?prefix }";
 	$.ajax({
@@ -13,10 +16,11 @@ $(document).ready(function(){
 			var bindings = data.results.bindings;
 			for (var i = 0; i < bindings.length; i++){
 				var binding = bindings[i];
+				namespaces[binding["prefix"].value] = binding["ns"].value;
 				queryTemplate.push("PREFIX ", binding["prefix"].value, ": <", binding["ns"].value, ">\n");
 			}
 			
-			queryTemplate.push("\nSELECT ?value \nWHERE { ?item rdf:value ?value }");
+			queryTemplate.push("\nSELECT ?value \nWHERE { \n?item rdf:value ?value . \n}");
 			
 			$("#query").val(queryTemplate.join(""));
 		}
@@ -50,11 +54,24 @@ $(document).ready(function(){
 				
 				// body
 				html.push("<tbody>");
+				var lastColumns = [];
 				for (var i = 0; i < bindings.length; i++){
 					var binding = bindings[i];
 					html.push("<tr>");
-					for (var key in binding){						
-						html.push("<td class='"+binding[key].type+"'>" + binding[key].value + "</td>");
+					for (var j = 0; j < vars.length; j++){
+						var key = vars[j];
+						if (lastColumns[j] != null && lastColumns[j] == binding[key].value) {
+							binding[key].value = "";
+						} else {
+							lastColumns[j] = binding[key].value;
+						}
+						if ("uri" == binding[key].type) {
+							html.push("<td class='"+binding[key].type+"'>" + printURI(binding[key].value) + "</td>");
+						} else if ("literal" == binding[key].type) {
+							html.push("<td class='"+binding[key].type+"'>" + binding[key].value.replace(/\n/g, "</br>") + "</td>");
+ 						} else {
+							html.push("<td class='"+binding[key].type+"'>" + binding[key].value + "</td>");
+						}
 					}
 					html.push("</tr>");
 				}
@@ -67,4 +84,16 @@ $(document).ready(function(){
 		return false;
 	});
 });			
-			
+
+function printURI(uri) {
+	if (uri == "") {
+		return "";
+	} else {
+		for (var key in namespaces){
+			if (uri.startsWith(namespaces[key])) {
+				return key + ":" + uri.substring(namespaces[key].length);
+			}
+		}
+		return "<" + uri + ">";
+	}
+}
