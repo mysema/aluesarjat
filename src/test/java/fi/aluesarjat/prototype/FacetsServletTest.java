36c30c62488346aae8da759f5c0eb4e89cc9c525
Timo Westkämper <timo.westkamper@mysema.com>
2010-11-24 16:12:22 +0000

    added test stubs

--- src/test/java/fi/aluesarjat/prototype/FacetsServletTest.java
+++ src/test/java/fi/aluesarjat/prototype/FacetsServletTest.java
@@ -4,61 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import javax.servlet.ServletException;
 
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
 
-import com.mysema.rdfbean.model.io.Format;
-import com.mysema.rdfbean.model.io.RDFSource;
-import com.mysema.rdfbean.sesame.MemoryRepository;
-import com.mysema.stat.scovo.NamespaceHandler;
-import com.mysema.stat.scovo.SCV;
-
-public class FacetsServletTest {
-
-    private static MemoryRepository repository;
-
-    private static FacetsServlet servlet;
-
-    private MockHttpServletRequest request;
-
-    private MockHttpServletResponse response;
-
-    @BeforeClass
-    public static void setUpClass() throws ServletException, IOException{
-        repository = new MemoryRepository();
-        repository.setSources(new RDFSource[]{
-                new RDFSource("classpath:/alue.ttl", Format.TURTLE, "http://localhost:8080/rdf/dimensions/Alue"),
-                new RDFSource("classpath:/scovo.rdf", Format.RDFXML, SCV.NS),
-                new RDFSource("classpath:/stat.rdf", Format.RDFXML, "http://data.mysema.com/rdf/pcaxis#")});
-        repository.initialize();
-
-        NamespaceHandler namespaceHandler = new NamespaceHandler(repository);
-        DataService dataService = new DataService(repository, namespaceHandler, "http://localhost:8080/rdf/", "true");
-        dataService.setDatasets(Collections.singletonList("A01HKIS_Vaestotulot \".\""));
-        dataService.setSequential(true);
-        dataService.initialize();
-
-        servlet = new FacetsServlet(repository);
-    }
-
-    @AfterClass
-    public static void tearDownClass(){
-        repository.close();
-    }
-
-    @Before
-    public void setUp(){
-        request = new MockHttpServletRequest();
-        response = new MockHttpServletResponse();
-    }
+public class FacetsServletTest extends AbstractFacetSearchServletTest{
 
     @Test
     public void Initial() throws ServletException, IOException{
---
+++ src/test/java/fi/aluesarjat/prototype/SearchServletTest.java
@@ -0,0 +1,13 @@
+package fi.aluesarjat.prototype;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SearchServletTest {
+
+    @Test
+    public void test(){
+        Assert.fail("No tests here");
+    }
+
+}
