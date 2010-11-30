f63c3fec126efcdca50ff1fa861b44806d340ca2
Timo Westkämper <timo.westkamper@mysema.com>
2010-11-30 14:27:56 +0000

    added test for namespace insertion

--- src/test/java/com/mysema/stat/scovo/NamespaceHandlerTest.java
+++ src/test/java/com/mysema/stat/scovo/NamespaceHandlerTest.java
@@ -1,5 +1,7 @@
 package com.mysema.stat.scovo;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +9,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.mysema.commons.lang.IteratorAdapter;
+import com.mysema.rdfbean.model.LIT;
+import com.mysema.rdfbean.model.RDFConnection;
+import com.mysema.rdfbean.model.UID;
 import com.mysema.rdfbean.sesame.MemoryRepository;
 import com.mysema.rdfbean.sesame.SesameRepository;
 import com.mysema.stat.META;
@@ -46,5 +52,38 @@ public class NamespaceHandlerTest {
         namespaces.put(baseURI + RDFDatasetHandler.DATASET_CONTEXT_BASE, "dataset");
         namespaceHandler.addNamespaces(namespaces);
     }
+
+    @Test
+    public void Overwrite_Namespace(){
+        NamespaceHandler namespaceHandler = new NamespaceHandler(repository);
+        Map<String,String> namespaces = new HashMap<String,String>();
+        namespaces.put(SCV.NS, "s");
+        namespaceHandler.addNamespaces(namespaces);
+
+        RDFConnection conn = repository.openConnection();
+        try{
+            assertEquals(1, IteratorAdapter.asList(conn.findStatements(null,            META.nsPrefix, null, null, false)).size());
+            assertEquals(1, IteratorAdapter.asList(conn.findStatements(null,            META.nsPrefix, new LIT("s"), null, false)).size());
+            assertEquals(1, IteratorAdapter.asList(conn.findStatements(new UID(SCV.NS), META.nsPrefix, null, null, false)).size());
+        }finally{
+            conn.close();
+        }
+
+        // overwrite
+        namespaces.put(SCV.NS, "scv");
+        namespaceHandler.addNamespaces(namespaces);
+
+        conn = repository.openConnection();
+        try{
+            assertEquals(1, IteratorAdapter.asList(conn.findStatements(null,            META.nsPrefix, null, null, false)).size());
+            assertEquals(0, IteratorAdapter.asList(conn.findStatements(null,            META.nsPrefix, new LIT("s"), null, false)).size());
+            assertEquals(1, IteratorAdapter.asList(conn.findStatements(null,            META.nsPrefix, new LIT("scv"), null, false)).size());
+            assertEquals(1, IteratorAdapter.asList(conn.findStatements(new UID(SCV.NS), META.nsPrefix, null, null, false)).size());
+        }finally{
+            conn.close();
+        }
+
+    }
+
 
 }
