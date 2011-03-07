262df1ec170c06913281bea3ef5338cb75f9613f
Timo Westkämper <timo.westkamper@mysema.com>
2011-03-07 13:09:34 +0000

    commented unused variables out

--- src/main/java/fi/aluesarjat/prototype/AreaDataServlet.java
+++ src/main/java/fi/aluesarjat/prototype/AreaDataServlet.java
@@ -26,29 +26,31 @@ import com.mysema.rdfbean.model.SPARQLQuery;
 import com.mysema.rdfbean.model.UID;
 
 public class AreaDataServlet extends AbstractSPARQLServlet{
-    
+
     private static final Logger log = LoggerFactory.getLogger(AbstractFacetSearchServlet.class);
 
     private static final long serialVersionUID = 1307767271709547227L;
-    
+
     private final Repository repository;
-    
+
     private final String baseURI;
-    
-    private final UID hel1, hel2, van1, van2, esp1, esp2;
-    
+
+//    private final UID hel1, hel2, van1, van2, esp1, esp2;
+    private final UID hel1, van1, esp1;
+
+
     public AreaDataServlet(Repository repository, String baseURI) {
         this.repository = repository;
         this.baseURI = baseURI;
         hel1 = new UID(baseURI + "datasets#", "A02S_HKI_Vakiluku1962");
         esp1 = new UID(baseURI + "datasets#", "B02S_ESP_Vakiluku1975");
-        van1 = new UID(baseURI + "datasets#", "C02S_VAN_Vakiluku1971");        
-        hel2 = new UID(baseURI + "datasets#", "A01HKI_Astuot_hper_rahoitus_talotyyppi");
-        esp2 = new UID(baseURI + "datasets#", "B01ESP_Asuntotuotanto_1997_");
-        van2 = new UID(baseURI + "datasets#", "C01VAN_Astuot_hper_rahoitus_talotyyppi");
-        
+        van1 = new UID(baseURI + "datasets#", "C02S_VAN_Vakiluku1971");
+//        hel2 = new UID(baseURI + "datasets#", "A01HKI_Astuot_hper_rahoitus_talotyyppi");
+//        esp2 = new UID(baseURI + "datasets#", "B01ESP_Asuntotuotanto_1997_");
+//        van2 = new UID(baseURI + "datasets#", "C01VAN_Astuot_hper_rahoitus_talotyyppi");
+
     }
-    
+
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest)req;
@@ -64,14 +66,14 @@ public class AreaDataServlet extends AbstractSPARQLServlet{
         }else if (areaId.startsWith("_092")){
             bindings.put("g", van1);
         }else{
-            bindings.put("g", hel1);    
+            bindings.put("g", hel1);
         }
-        
-        RDFConnection connection = repository.openConnection();        
+
+        RDFConnection connection = repository.openConnection();
         try{
             JSONArray result = new JSONArray();
             //result.put("id", areaId);
-            
+
             // väkiluku
             StringBuilder query = new StringBuilder();
             query.append("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n");
@@ -79,16 +81,16 @@ public class AreaDataServlet extends AbstractSPARQLServlet{
             query.append("PREFIX dimension: <"+baseURI+"dimensions/>\n");
             query.append("PREFIX scv: <http://purl.org/NET/scovo#> \n");
             query.append("PREFIX vuosi: <"+baseURI+"dimensions/Vuosi#>\n");
-            
+
             query.append("SELECT ?ikl ?val \nWHERE { \n");
             query.append(" GRAPH ?g { \n");
-            query.append("  ?item scv:dimension vuosi:_2009 , ?area , ?ik ; rdf:value ?val . \n");           
+            query.append("  ?item scv:dimension vuosi:_2009 , ?area , ?ik ; rdf:value ?val . \n");
             query.append(" } \n");
             query.append(" ?ik rdf:type dimension:Ikäryhmä ; dc:title ?ikl \n");
             query.append("}");
             if (log.isInfoEnabled()){
-                log.info(query.toString());    
-            }            
+                log.info(query.toString());
+            }
             SPARQLQuery sparqlQuery = connection.createQuery(QueryLanguage.SPARQL, query.toString());
             sparqlQuery.setBinding("area", bindings.get("area"));
             sparqlQuery.setBinding("g", bindings.get("g"));
@@ -106,15 +108,15 @@ public class AreaDataServlet extends AbstractSPARQLServlet{
             }finally{
                 sparqlResult.close();
             }
-            
+
 //            if (areaId.startsWith("_049")){
 //                bindings.put("g", esp2);
 //            }else if (areaId.startsWith("_092")){
 //                bindings.put("g", van2);
 //            }else{
-//                bindings.put("g", hel2);    
+//                bindings.put("g", hel2);
 //            }
-//            
+//
 //            // asuntotuotanto (lukumäärä)
 //            query = new StringBuilder();
 //            query.append("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n");
@@ -127,27 +129,27 @@ public class AreaDataServlet extends AbstractSPARQLServlet{
 //            query.append("PREFIX rahoitusmuoto: <"+baseURI+"dimensions/Rahoitusmuoto#>\n");
 //            query.append("PREFIX talotyyppi: <"+baseURI+"dimensions/Talotyyppi#>\n");
 //            query.append("PREFIX scv: <http://purl.org/NET/scovo#> \n");
-//            
+//
 //            query.append("SELECT ?val \nWHERE {\n");
 //            query.append(" GRAPH ?g {\n");
 //            query.append("  ?item scv:dimension ?area , yksikkö:Asuntojen_lukumäärä , hallintaperuste:Asunnot_yhteensä , " +
 //                         "  rahoitusmuoto:Yhteensä , vuosi:_2009, talotyyppi:Yhteensä, huoneistotyyppi:Yhteensä ; \n");
 //            query.append("  rdf:value ?val . \n");
 //            query.append(" }\n");
-//            query.append("}");        
+//            query.append("}");
 //            if (log.isInfoEnabled()){
-//                log.info(query.toString());    
-//            }            
+//                log.info(query.toString());
+//            }
 //            start = System.currentTimeMillis();
 //            NODE node = getSingleResult(connection, query.toString(), bindings);
 //            System.err.println((System.currentTimeMillis()-start)+"ms");
 //            if (node != null){
 //                JSONObject entry = new JSONObject();
 //                entry.put("label", "Asuntotuotanto (lkm)");
-//                entry.put("value", node.getValue());                
+//                entry.put("value", node.getValue());
 //                result.add(entry);
-//            }       
-//            
+//            }
+//
 //            // asuntotuotanto (pinta-ala)
 //            query = new StringBuilder();
 //            query.append("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n");
@@ -160,27 +162,27 @@ public class AreaDataServlet extends AbstractSPARQLServlet{
 //            query.append("PREFIX rahoitusmuoto: <"+baseURI+"dimensions/Rahoitusmuoto#>\n");
 //            query.append("PREFIX talotyyppi: <"+baseURI+"dimensions/Talotyyppi#>\n");
 //            query.append("PREFIX scv: <http://purl.org/NET/scovo#> \n");
-//            
+//
 //            query.append("SELECT ?val \nWHERE {\n");
 //            query.append(" GRAPH ?g {\n");
 //            query.append("  ?item scv:dimension ?area , yksikkö:Asuntojen_pinta-ala_m2 , hallintaperuste:Asunnot_yhteensä , " +
 //                         "  rahoitusmuoto:Yhteensä , vuosi:_2009, talotyyppi:Yhteensä, huoneistotyyppi:Yhteensä ; \n");
 //            query.append("  rdf:value ?val . \n");
 //            query.append(" }\n");
-//            query.append("}");            
+//            query.append("}");
 //            if (log.isInfoEnabled()){
-//                log.info(query.toString());    
-//            }            
+//                log.info(query.toString());
+//            }
 //            start = System.currentTimeMillis();
 //            node = getSingleResult(connection, query.toString(), bindings);
 //            System.err.println((System.currentTimeMillis()-start)+"ms");
 //            if (node != null){
-//                JSONObject entry = new JSONObject();                
+//                JSONObject entry = new JSONObject();
 //                entry.put("label", "Asuntotuotanto (pinta-ala)");
 //                entry.put("value", node.getValue());
 //                result.add(entry);
-//            }   
-            
+//            }
+
             Writer out = response.getWriter();
             String jsonpCallback = request.getParameter("jsonp");
             if (jsonpCallback != null){
@@ -188,10 +190,10 @@ public class AreaDataServlet extends AbstractSPARQLServlet{
                 result.write(out);
                 out.write(")");
             }else{
-                result.write(out);    
+                result.write(out);
             }
             out.flush();
-            
+
         }finally{
             connection.close();
         }
